20968788c13563a8642186e1cff0534a8dc61940
Ryan Flynn <ryanflynn44@gmail.com>
2020-07-22 11:50:45 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,1 @@
+CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age AGE, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN)
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Mr. Chocolate", 8, "M", "Brown", "goofy", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Rowdy", 6, "M", "Brown", "rowdy", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Tabitha", 6, "F", "Black", "goofy", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Sergeant Brown", 4, "F", "Brown", "intense", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Melissa", 3, "F", "Brown", "goofy", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Grinch", 9, "F", "Brown", "aggressive", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Wendy", 2, "F", "Black", "goofy", 1);
+INSERT INTO bears(age, gender, color, temperament, alive) VALUES( 2, "M", "Black", "aggressive", 0);
