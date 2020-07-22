948be4a7ad1d0ba952dff387423e774f2d209dad
kevin_xie <xie.kevin.j@gmail.com>
2020-07-22 18:35:42 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+-- Use the CREATE TABLE command to create a new table with various data types
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+-- Use the INSERT INTO command to insert data (i.e. rows) into a database table
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 21, "M", "Brown", "sad", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 22, "M", "Brown", "aggro", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 22, "M", "Brown", "aggro", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 24, "M", "Brown", "aggro", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 421, "M", "Brown", "aggro", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 232, "M", "Brown", "aggro", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 211, "M", "Brown", "aggro", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 321, "F", "Brown", "aggro", TRUE);
